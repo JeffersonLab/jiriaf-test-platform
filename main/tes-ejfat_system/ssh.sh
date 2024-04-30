77a813d4781e82817183be582737f8e8dbd767fa
tsaie79 <tsaie79@gmail.com>
2024-04-30 17:43:26 +0000

    Add SSH port forwarding for ejfat target in ssh.sh and prometheus.yml

---
+++ main/tes-ejfat_system/ssh.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# # this should be run at host not ejfat
+
+
+
+# export KUBELET_PORT="10250"
+# ssh -NfL *:$KUBELET_PORT:localhost:$KUBELET_PORT ejfat-fs
+
+
+
+# #prom process-exporter ersap-1
+# export PROCESS_EXPORTER_PORT="1776"
+# ssh -NfL $PROCESS_EXPORTER_PORT:localhost:$PROCESS_EXPORTER_PORT ejfat-fs 
+
+# # k8s: stress
+# export STRESS_PORT="12345"
+# ssh -NfL $STRESS_PORT:localhost:$STRESS_PORT ejfat-fs
+
+# # prom ersap
+# ## 4 pipe
+# for i in {2221..2224}
+# do
+#     j=$((i + 10000))
+#     ssh -NfL $j:localhost:$i ejfat-fs
+# done
+
+## 1 pipe
+ssh -NfL 2221:localhost:2221 ejfat-6
+ssh -NfL 8080:localhost:8080 ejfat-6
