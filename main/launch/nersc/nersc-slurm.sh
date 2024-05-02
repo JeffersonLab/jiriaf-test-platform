45760328bc1b2f819dc123c4f37e8edffde97f10
tsaie79 <tsaie79@gmail.com>
2024-05-02 15:54:15 +0000

    Add new scripts for launching nodes and submitting Slurm jobs at NERSC

---
+++ main/launch/nersc/nersc-slurm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -N 2
+#SBATCH -C cpu
+#SBATCH -q debug
+#SBATCH -J 100g
+#SBATCH -t 00:30:00
+
+#run the application:
+
+for i in $(seq 1 2)
+do
+    srun -N1 /global/homes/j/jlabtsai/run-vk/slurm/node_general.sh $i &
+done
+
+wait
