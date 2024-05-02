33679994e584d5082e1011acdd1ea4bb983b1339
tsaie79 <tsaie79@gmail.com>
2024-05-02 16:46:59 +0000

    Update number of nodes in nersc-slurm.sh for launching nodes at NERSC

--- main/launch/mylin/nersc-slurm.sh
+++ main/launch/mylin/nersc-slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -N 1
+#SBATCH -N 2
 #SBATCH -C cpu
 #SBATCH -q debug
 #SBATCH -J 100g
@@ -8,7 +8,7 @@
 
 #run the application:
 
-for i in $(seq 1)
+for i in $(seq 1 2)
 do
     srun -N1 /global/homes/j/jlabtsai/run-vk/slurm/mylin/node-setup.sh $i &
 done
