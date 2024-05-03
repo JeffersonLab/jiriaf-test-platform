fbd813ac67a211b1f1cfb7b3733f6c470574066d
tsaie79 <tsaie79@gmail.com>
2024-05-03 16:23:24 +0000

    Add helm commands to install and uninstall 25 ersap instances

--- main/launch/mylin/job/cmd.sh
+++ main/launch/mylin/job/cmd.sh
@@ -13,3 +13,9 @@ while true; do kubectl get node; echo ----; kubectl get pods -n 100g; echo ===; 
 
 # for helm
 helm install ersap1 `pwd` --set name=1
+
+# install 25 ersap instances
+for i in $(seq 1 25); do helm install ersap$i `pwd` --set name=$i; 
+
+# remove 25 ersap instances
+for i in $(seq 1 25); do helm uninstall ersap$i; done
