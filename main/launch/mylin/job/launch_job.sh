b44a62afc1fbeea789ffe7e4b577e2497237df32
tsaie79 <tsaie79@gmail.com>
2024-05-03 17:21:29 +0000

    Refactor launch_job.sh script to use padded index in helm install and uninstall commands

--- main/launch/mylin/job/launch_job.sh
+++ main/launch/mylin/job/launch_job.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-
+# test via template
+for i in $(seq 1 25)
+do
+    i_padded=$(printf "%02d" $i)
+    echo $i_padded
+    helm template ersap$i ersap --set name=$i_padded
+    sleep 5
+done
 
 # gradually increase the number of ersap instances every 5 seconds
 for i in $(seq 1 25)
 do
     i_padded=$(printf "%02d" $i)
     echo $i_padded
-    helm install ersap$i ersap --set name=ersap$i_padded
+    helm install ersap$i ersap --set name=$i_padded
     sleep 30
 done
 
@@ -16,12 +23,9 @@ done
 # gradually remove the number of ersap instances every 5 seconds
 for i in $(seq 1 25)
 do
-    i_padded=$(printf "%02d" $i)
-    echo $i_padded
     helm uninstall ersap$i
     sleep 30
 done
-
 
 
 
