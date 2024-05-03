64dedec57b4dcbfe4c1f605dee2e5bc83ff197e4
tsaie79 <tsaie79@gmail.com>
2024-05-03 16:19:09 +0000

    Add Prometheus configuration for scraping additional targets

---
+++ prom/create_prom_config.py
@@ -0,0 +1,40 @@
+import yaml
+
+# Open the YAML file and load its contents.
+with open('prometheus-test.yml', 'r') as file:
+    data = yaml.safe_load(file)
+
+# Loop from 1 to 25.
+for i in range(1, 26):
+    # Format i as a two-digit number.
+    i_padded = f"{i:02d}"
+
+    # Define the new jobs.
+    new_jobs = [
+        {
+            'job_name': f'j-vk-{i_padded}',
+            'scheme': 'https',
+            'static_configs': [{'targets': [f'localhost:100{i_padded}']}],
+            'metrics_path': '/metrics/resource',
+            'tls_config': {'insecure_skip_verify': True}
+        },
+        {
+            'job_name': f'j-ersap-process-exporter-{i_padded}',
+            'static_configs': [{'targets': [f'localhost:200{i_padded}']}],
+        },
+        {
+            'job_name': f'vd-ersap-{i_padded}',
+            'static_configs': [{'targets': [f'localhost:300{i_padded}']}],
+        },
+        {
+            'job_name': f'vd-ejfat-{i_padded}',
+            'static_configs': [{'targets': [f'localhost:400{i_padded}']}],
+        }
+    ]
+
+    # Add the new jobs to the scrape_configs key.
+    data['scrape_configs'].extend(new_jobs)
+
+# Write the modified data back to the YAML file.
+with open('prometheus-test.yml', 'w') as file:
+    yaml.safe_dump(data, file)
