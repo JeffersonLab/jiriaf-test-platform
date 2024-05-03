341dcbe6564f1b38b69a75e2dad76f38b6a8ab9a
tsaie79 <tsaie79@gmail.com>
2024-05-03 18:36:00 +0000

    Update Prometheus configuration to include additional scrape targets and adjust num_proc_nodes label

--- prom/create_prom_config/create_prom_config.py
+++ prom/create_prom_config/create_prom_config.py
@@ -4,10 +4,22 @@ import yaml
 with open('/prom/prometheus-temp.yml', 'r') as file:
     data = yaml.safe_load(file)
 
+# num_proc_nodes
+num_proc_nodes = 25
+
+# global labels
+global_labels = {
+    "task_name": "ersap-100g",
+    "num_proc_nodes": str(num_proc_nodes),
+    "comupte_site": "nersc",
+    "run_type": "production",
+    "run_date": "2024-05-02",
+}
+
 data['scrape_configs'] = []
 
 # Loop from 1 to 25.
-for i in range(1, 4):
+for i in range(1, num_proc_nodes + 1):
     # Format i as a two-digit number.
     i_padded = f"{i:02d}"
 
@@ -16,21 +28,22 @@ for i in range(1, 4):
         {
             'job_name': f'j-vk-{i_padded}',
             'scheme': 'https',
-            'static_configs': [{'targets': [f'localhost:100{i_padded}']}],
+            'static_configs': [{'targets': [f'localhost:100{i_padded}'], 'labels': global_labels.copy()}],
             'metrics_path': '/metrics/resource',
-            'tls_config': {'insecure_skip_verify': True}
+            'tls_config': {'insecure_skip_verify': True},
         },
         {
             'job_name': f'j-ersap-process-exporter-{i_padded}',
-            'static_configs': [{'targets': [f'localhost:200{i_padded}']}],
+            'static_configs': [{'targets': [f'localhost:200{i_padded}'], 'labels': global_labels.copy()}],
+
         },
         {
             'job_name': f'vd-ersap-{i_padded}',
-            'static_configs': [{'targets': [f'localhost:300{i_padded}']}],
+            'static_configs': [{'targets': [f'localhost:300{i_padded}'], 'labels': global_labels.copy()}],
         },
         {
             'job_name': f'vd-ejfat-{i_padded}',
-            'static_configs': [{'targets': [f'localhost:400{i_padded}']}],
+            'static_configs': [{'targets': [f'localhost:400{i_padded}'], 'labels': global_labels.copy()}],
         }
     ]
 
