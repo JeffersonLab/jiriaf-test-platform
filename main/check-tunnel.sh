5c346e7f3fb4707a5ec73f434391b88694038f4d
tsaie79 <tsaie79@gmail.com>
2024-05-06 16:06:29 +0000

    Refactor check-tunnel.sh script to check SSH tunnel on multiple ports

--- main/check-tunnel.sh
+++ main/check-tunnel.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-alias psp="ps -u $USER -o pid,ppid,sid,pgid,%cpu,%mem,cmd"
-
-psp | grep localhost
+for i in $(seq 1 30)
+do
+    i_padded=$(printf "%02d" $i)
+    for prefix in 100 200 300 400
+    do
+        port=$prefix$i_padded
+        nc -z localhost $port &> /dev/null
+        if [ $? -ne 0 ]; then
+            echo "SSH tunnel on port $port is not open"
+        fi
+    done
+done
