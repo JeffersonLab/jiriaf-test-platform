b1a20c0510b0dea58ce24a2023203ad064704fe2
tsaie79 <tsaie79@gmail.com>
2024-05-20 05:49:57 +0000

    Increase number of ersap instances to 40 in nersc-slurm.sh

--- main/nersc-slurm.sh
+++ main/nersc-slurm.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#SBATCH -N 25
-#SBATCH -C cpu
-#SBATCH -q regular
-#SBATCH -J 100g
-#SBATCH -t 01:00:00
+## SBATCH -N 40
+## SBATCH -C cpu
+## SBATCH -q regular
+## SBATCH -J 100g
+## SBATCH -t 03:00:00
+#SBATCH --reservation=100g
 
 #run the application:
 
-for i in $(seq 1 30)
+for i in $(seq 1 40)
 do
     i_padded=$(printf "%02d" $i)
     echo $i_padded
