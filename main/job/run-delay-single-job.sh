6aebe844722d1ace0a97f382be43c46d1dead851
tsaie79 <tsaie79@gmail.com>
2024-05-31 23:36:03 -0400

    update

--- main/job/run-delay-single-job.sh
+++ main/job/run-delay-single-job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define the number of pods you want to create
-num_pods=6
+num_pods=2
 
 # Loop to create the pods
 for ((i=1; i<=num_pods; i++)); do
   # Use Helm to install the job
-  helm install job$i ersap-helm/single-job/ --set Deployment.name=job$i
+  helm install ersap-test3-job$i ersap-helm/single-job/ --set Deployment.name=ersap-test3-job$i
 
   # Wait for 30 seconds before creating the next pod
   sleep 30
