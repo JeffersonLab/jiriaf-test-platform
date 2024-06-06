9083ac493faaf02fac9ab9f93f37566fa8b06b99
tsaie79 <tsaie79@gmail.com>
2024-06-06 13:33:35 -0400

    add proxy_remote

--- main/build-ssh-at-login.sh
+++ main/build-ssh-at-login.sh
@@ -7,22 +7,25 @@
 
 
 # run this on jiriaf2301 to login04 at nersc
+
+export proxy_remote="jlabtsai@128.55.64.25"
+
 export APISERVER_PORT="38687" #"35679"
-ssh -i ~/.ssh/nersc -J perlmutter -NfR $APISERVER_PORT:localhost:$APISERVER_PORT jlabtsai@128.55.64.13
+ssh -i ~/.ssh/nersc -J perlmutter -NfR $APISERVER_PORT:localhost:$APISERVER_PORT $proxy_remote
 
 
 
-for i in $(seq 1 40)
+for i in $(seq 1 1)
 do
     i_padded=$(printf "%02d" $i)
     echo "100""$i_padded"
-    ssh -i ~/.ssh/nersc -J perlmutter -NfL 100$i_padded:localhost:100$i_padded jlabtsai@128.55.64.13
+    ssh -i ~/.ssh/nersc -J perlmutter -NfL *:100$i_padded:localhost:100$i_padded $proxy_remote
     echo "200""$i_padded"
-    ssh -i ~/.ssh/nersc -J perlmutter -NfL 200$i_padded:localhost:200$i_padded jlabtsai@128.55.64.13
+    ssh -i ~/.ssh/nersc -J perlmutter -NfL *:200$i_padded:localhost:200$i_padded $proxy_remote
     echo "300""$i_padded"
-    ssh -i ~/.ssh/nersc -J perlmutter -NfL 300$i_padded:localhost:300$i_padded jlabtsai@128.55.64.13
+    ssh -i ~/.ssh/nersc -J perlmutter -NfL *:300$i_padded:localhost:300$i_padded $proxy_remote
     echo "400""$i_padded"
-    ssh -i ~/.ssh/nersc -J perlmutter -NfL 400$i_padded:localhost:400$i_padded jlabtsai@128.55.64.13
+    ssh -i ~/.ssh/nersc -J perlmutter -NfL *:400$i_padded:localhost:400$i_padded $proxy_remote
 done
 
 wait
