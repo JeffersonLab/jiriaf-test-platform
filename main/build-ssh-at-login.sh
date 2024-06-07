ea03ca760df70cc7856574fb7316b655176967c4
tsaie79 <tsaie79@gmail.com>
2024-06-06 20:57:35 -0400

    update

--- main/build-ssh-at-login.sh
+++ main/build-ssh-at-login.sh
@@ -8,14 +8,14 @@
 
 # run this on jiriaf2301 to login04 at nersc
 
-export proxy_remote="jlabtsai@128.55.64.25"
+export proxy_remote="jlabtsai@128.55.64.38"
 
 export APISERVER_PORT="38687" #"35679"
 ssh -i ~/.ssh/nersc -J perlmutter -NfR $APISERVER_PORT:localhost:$APISERVER_PORT $proxy_remote
 
 
 
-for i in $(seq 1 1)
+for i in $(seq 0 0)
 do
     i_padded=$(printf "%02d" $i)
     echo "100""$i_padded"
