70ac90ae027e561c9bcf57d3724b3bd06a022146
tsaie79 <tsaie79@gmail.com>
2024-06-11 14:20:51 -0400

    add how to run source of ejfat nodes

---
+++ main/run-source.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+image=gurjyan/ersap:v0.1
+
+#input_volume=$((1024*))
+
+docker run -it --rm --net=host -v /daqfs/java/clas_005038.1231.hipo:/clas_005038.1231.hipo \
+    --entrypoint "/bin/bash" -e bufrate=$input_volume $image -c 'source ./setup; clasBlasterNTP -f /clas_005038.1231.hipo -file ./ejfat_uri'
+
+# -bufrate=$bufrate to adjust volume
