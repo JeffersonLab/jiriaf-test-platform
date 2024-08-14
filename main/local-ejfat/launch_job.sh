cbbf274cb6b336077176978cdb865bffe7224027
jeng-yuantsai <tsaie79@gmail.com>
2024-08-14 17:27:38 -0500

    Update launch_job.sh script with service names and container ports

--- main/local-ejfat/launch_job.sh
+++ main/local-ejfat/launch_job.sh
@@ -16,24 +16,37 @@ PROCESS_EXPORTER_PORT=$((ERSAP_EXPORTER_PORT + 1))
 EJFAT_EXPORTER_PORT=$((ERSAP_EXPORTER_PORT + 2))
 ERSAP_QUEUE_PORT=$((ERSAP_EXPORTER_PORT + 3))
 
-# Function to update a specific port in values.yaml
-update_port() {
-  local service_index=$1
-  local new_port=$2
-  awk -v idx="$service_index" -v port="$new_port" '
-    $1 == "Service["idx"].port:" { $2 = port }
-    { print }
-  ' values.yaml > values.tmp && mv values.tmp values.yaml
-}
-
-# Update values.yaml
-update_port 0 $ERSAP_EXPORTER_PORT
-update_port 1 $PROCESS_EXPORTER_PORT
-update_port 2 $EJFAT_EXPORTER_PORT
-update_port 3 $JRM_EXPORTER_PORT
-update_port 4 $ERSAP_QUEUE_PORT
-
 # Run Helm install
 helm install "$ID-job-$INDEX" job/ \
   --set Deployment.name="$ID-job-$INDEX" \
-  --set Deployment.serviceMonitorLabel=$ID
+  --set Deployment.serviceMonitorLabel=$ID \
+  --set Service[0].name="ersap-exporter" \
+  --set Service[0].protocol="TCP" \
+  --set Service[0].port="$ERSAP_EXPORTER_PORT" \
+  --set Service[0].originalPort=2221 \
+  --set Service[0].path="/metrics" \
+  --set Service[0].interval="15s" \
+  --set Service[1].name="process-exporter" \
+  --set Service[1].protocol="TCP" \
+  --set Service[1].port="$PROCESS_EXPORTER_PORT" \
+  --set Service[1].originalPort=1776 \
+  --set Service[1].path="/metrics" \
+  --set Service[1].interval="15s" \
+  --set Service[2].name="ejfat-exporter" \
+  --set Service[2].protocol="TCP" \
+  --set Service[2].port="$EJFAT_EXPORTER_PORT" \
+  --set Service[2].originalPort=8088 \
+  --set Service[2].path="/metrics" \
+  --set Service[2].interval="15s" \
+  --set Service[3].name="jrm-exporter" \
+  --set Service[3].protocol="TCP" \
+  --set Service[3].port="$JRM_EXPORTER_PORT" \
+  --set Service[3].originalPort=10000 \
+  --set Service[3].path="/metrics/resource" \
+  --set Service[3].interval="15s" \
+  --set Service[4].name="ersap-queue" \
+  --set Service[4].protocol="TCP" \
+  --set Service[4].port="$ERSAP_QUEUE_PORT" \
+  --set Service[4].originalPort=2222 \
+  --set Service[4].path="/metrics/resource" \
+  --set Service[4].interval="15s"
