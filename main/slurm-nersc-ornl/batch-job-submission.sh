a881555c086b2af65e3f8f8d61e38a266fbd4bb9
jeng-yuantsai <tsaie79@gmail.com>
2024-08-15 08:31:46 -0500

    Refactor batch-job-submission.sh script to improve readability and add support for multiple job submissions

---
+++ main/slurm-nersc-ornl/batch-job-submission.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Base values
+ID="jlab-100g-nersc-ornl"
+SITE="perlmutter" #ornl
+ERSAP_EXPORTER_PORT_BASE=20000 #20160
+JRM_EXPORTER_PORT_BASE=10000 #10040
+
+# Total number from argument
+TOTAL_NUMBER=2
+
+# Loop to run the script from 0 to total number - 1
+for ((i=0; i<TOTAL_NUMBER; i++)); do
+  INDEX="perlmutter-$i"
+  ERSAP_EXPORTER_PORT=$((ERSAP_EXPORTER_PORT_BASE + i * 4))
+  JRM_EXPORTER_PORT=$((JRM_EXPORTER_PORT_BASE + i))
+  
+  # print the values
+  echo "ID: $ID, INDEX: $INDEX, SITE: $SITE, ERSAP_EXPORTER_PORT: $ERSAP_EXPORTER_PORT, JRM_EXPORTER_PORT: $JRM_EXPORTER_PORT"
+  ./launch_job.sh "$ID" "$INDEX" "$SITE" "$ERSAP_EXPORTER_PORT" "$JRM_EXPORTER_PORT"
+done
