7b7c231ce27d5e9323cdc64896895cc189e64aaf
tsaie79 <tsaie79@gmail.com>
2025-05-30 10:41:40 -0400

    Update batch-job-submission.sh for demo configuration on perlmutter with adjusted total number and fixed port values

--- main/slurm-nersc-ornl/batch-job-submission.sh
+++ main/slurm-nersc-ornl/batch-job-submission.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Base values
-ID="jlab-100g-nersc-ornl"
-SITE="ornl" #ornl
-ERSAP_EXPORTER_PORT_BASE=20000   #20160 #orig port = 2221
+ID="demo"
+SITE="perlmutter" #ornl
+ERSAP_EXPORTER_PORT_BASE=20000   #20160 #orig port = 2221 (after 10 nodes, 20040)
 JRM_EXPORTER_PORT_BASE=10000 #10040 #orig port = 10000
 
 # Total number from argument
-TOTAL_NUMBER=1
+TOTAL_NUMBER=17
 
 # Loop to run the script from 0 to total number - 1
-for ((i=0; i<TOTAL_NUMBER; i++)); do
+for ((i=16; i<TOTAL_NUMBER; i++)); do 
   INDEX="$SITE-$i"
-  ERSAP_EXPORTER_PORT=$((ERSAP_EXPORTER_PORT_BASE + i * 4))
-  JRM_EXPORTER_PORT=$((JRM_EXPORTER_PORT_BASE + i))
+  ERSAP_EXPORTER_PORT=20172 #$((ERSAP_EXPORTER_PORT_BASE + i * 4))
+  JRM_EXPORTER_PORT=10043 #$((JRM_EXPORTER_PORT_BASE + i))
   
   # print the values
   echo "ID: $ID, INDEX: $INDEX, SITE: $SITE, ERSAP_EXPORTER_PORT: $ERSAP_EXPORTER_PORT, JRM_EXPORTER_PORT: $JRM_EXPORTER_PORT"
